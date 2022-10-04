345a98c16bd87239f24c660e8d0aef98ea0080d1
Felicia Kenney <fkenney@bravocg.com>
2022-10-04 12:17:06 -0400

    binary conversion

--- Lab1.1_Starter/src/com/algorithms/BinaryConvert.java
+++ Lab1.1_Starter/src/com/algorithms/BinaryConvert.java
@@ -1,14 +1,24 @@
 package com.algorithms;
 
 public class BinaryConvert {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int number = convertFromBinary("100101");
         System.out.println("Number for binary 100101: " + number);
     }
 
-    public static int convertFromBinary(String binary){
+    public static int convertFromBinary(String binary) {
         // TODO-Lab1.1: return calculated decimal value converted from String binary
-        return 0;
+        int result = 0;
+        int conversion = 1;
+        for(int i = binary.length() -1; i >= 0 ; i--){
+            int num = Integer.parseInt(String.valueOf(binary.charAt(i)));
+            if (num == 1){
+                num *=conversion;
+                result += num;
+            }
+            conversion*=2;
+        }
+        return result;
     }
 
 }
